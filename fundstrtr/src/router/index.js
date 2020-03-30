5f3f34d2f7ebdd4612ca644f2a724d5ffc0521d3
Oluwaloni Richard <lonnyrichard27@gmail.com>
2020-03-30 15:55:14 +0100

    first commit [frontend]

--- fundstrtr/src/router/index.js
+++ fundstrtr/src/router/index.js
@@ -1,14 +1,15 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+// import Home from "../views/Home.vue";
+import Navbar from "../views/Navbar.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Navbar",
+    component: Navbar
   },
   {
     path: "/about",
--- fundstrtr/src/views/Navbar.vue
+++ fundstrtr/src/views/Navbar.vue
@@ -1,11 +1,13 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="/">
+            <img src="https://res.cloudinary.com/https-eazifunds-com/image/upload/v1585083673/Frame_nct3fp.png" alt="">
+        </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
   <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
+    <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
